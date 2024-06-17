072283d1498db5ed3d164bab903abfcd68d6c35f
Luqman Matloob <93182189+luqmanmatloob@users.noreply.github.com>
2024-06-17 20:16:45 +0500

    Test

--- client/src/components/InvoiceQuotesList.jsx
+++ client/src/components/InvoiceQuotesList.jsx
@@ -14,10 +14,12 @@ const InvoiceQuotesList = () => {
     const fetchInvoicesQuotes = async () => {
         try {
             const response = await fetch(`${BASE_URL}/api/invoicequote/allInvoicesQuotes`);
+
             if (!response.ok) {
                 throw new Error('Failed to fetch data');
             }
             const data = await response.json();
+            console.log(data)
             const reversedData = data.data.reverse();
             setInvoicesQuotes(reversedData);
         } catch (error) {
@@ -57,27 +59,29 @@ const InvoiceQuotesList = () => {
         <div className="
         container p-6 mx-auto bg-white rounded-lg shadow-2xl  my-5 border-b-slate-300 border-solid border-2"
         >
-            <h1 className="text-3xl font-bold mb-4">All Invoices and Quotes</h1>
+            <h1 className="text-3xl font-bold mb-4">All Invoices/Quotes</h1>
             <table className="table-auto w-full border-collapse border border-gray-300">
                 <thead>
                     <tr className="bg-gray-100">
-                        <th className="border border-gray-300 px-4 py-2">Unique Key</th>
+                        {/* <th className="border border-gray-300 px-4 py-2">Unique Key</th> */}
+                        <th className="border border-gray-300 px-4 py-2">Order Number </th>
                         <th className="border border-gray-300 px-4 py-2">Type</th>
-                        <th className="border border-gray-300 px-4 py-2">Invoice/Quote Number</th>
                         <th className="border border-gray-300 px-4 py-2">Date Ordered</th>
-                        <th className="border border-gray-300 px-4 py-2">Date Due</th>
+                        {/* <th className="border border-gray-300 px-4 py-2">Date Due</th> */}
                         <th className="border border-gray-300 px-4 py-2">Order Total</th>
+                        <th className="border border-gray-300 px-4 py-2">Edit</th>
+                        <th className="border border-gray-300 px-4 py-2">Delete</th>
                         {/* Add more headers as per your data structure */}
                     </tr>
                 </thead>
                 <tbody>
                     {invoicesQuotes.map((invoiceQuote) => (
-                        <tr key={invoiceQuote._id} className="bg-white">
-                            <td className="border border-gray-300 px-4 py-2">{invoiceQuote.uniqueKey}</td>
-                            <td className="border border-gray-300 px-4 py-2">{invoiceQuote.type}</td>
+                        <tr key={invoiceQuote._id} className="bg-white text-center">
+                            {/* <td className="border border-gray-300 px-4 py-2">{invoiceQuote.uniqueKey}</td> */}
                             <td className="border border-gray-300 px-4 py-2">{invoiceQuote.orderNumber}</td>
+                            <td className="border border-gray-300 px-4 py-2">{invoiceQuote.type}</td>
                             <td className="border border-gray-300 px-4 py-2">{formatDate(invoiceQuote.dateOrdered)}</td>
-                            <td className="border border-gray-300 px-4 py-2">{formatDate(invoiceQuote.dateDue)}</td>
+                            {/* <td className="border border-gray-300 px-4 py-2">{formatDate(invoiceQuote.dateDue)}</td> */}
                             <td className="border border-gray-300 px-4 py-2">{invoiceQuote.orderTotal}</td>
                             <td className="border border-gray-300 px-4 py-2"><Link to={`/Edit/${invoiceQuote.uniqueKey}`} className="text-blue-500 hover:underline mr-4">Edit</Link></td>
                             <td className="border border-gray-300 px-4 py-2"><button onClick={() => handleDelete(invoiceQuote.uniqueKey)} className="text-red-500 hover:underline">Delete</button></td>
