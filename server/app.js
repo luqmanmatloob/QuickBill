d8519f1b5accf148d61743914af31e10ead6716f
Luqman Matloob <93182189+luqmanmatloob@users.noreply.github.com>
2024-06-23 14:15:19 +0500

    test

--- server/app.js
+++ server/app.js
@@ -16,11 +16,11 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' })); // Adjust the limit as per your requirement
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.static('public'));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/invoiceDB', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
