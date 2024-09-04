5100fa9f47344b9f083fe07c42344865675bcebf
Luqman Matloob <93182189+luqmanmatloob@users.noreply.github.com>
2024-09-05 01:21:57 +0500

    updated header

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -7,20 +7,6 @@ import { useState } from 'react';
 
 const Header = () => {
 
-  const [toggleAccount, setToggleAccount] = useState(false);
-
-  const handleToggleAccount = () => {
-    if (toggleAccount) {
-      setToggleAccount(false);
-    } else {
-      setToggleAccount(true);
-    }
-  };
-
-
-
-
-
 
   return (
     <div className="fixed left-0 right-0 top-0 z-50 border-b-2 border-[#d1e4f5] bg-[#f5faff] py-3">
@@ -34,37 +20,23 @@ const Header = () => {
         </div>
 
 
-        <div>
-          <button className="relative flex items-center gap-2 rounded-md bg-blue-100 px-5 py-2 hover:bg-blue-300 font-Josefin-Sans cursor-pointers mt-1 text-sm font-bold uppercase"
+        <div className='flex items-center justify-center gap-5'>
 
-            onClick={handleToggleAccount}>
-            Account
-            <IoPersonOutline className="mb-1 text-lg font-semibold" />
+          <button
+            onClick={
+              () => {
+                localStorage.clear();
+                window.location.href = '/login';
+              }
+            }
+            className=' font-semibold text-blue-600 hover:text-blue-700  text-start text-lg hover:underline cursor-pointer'>
+            Logout
           </button>
 
-          <div className={`${toggleAccount ? 'block' : 'hidden'} ml-4 absolute right-8 top-[65px] bg-[#f5faff]  shadow-xl p-9 `}>
-            <div className='font-semibold hover:text-[#2046cf] hover:underline text-blue-600'>
-            <NavLink to="/accountsettings" >
-              <li className='list-none underline mb-3 cursor-pointer'>
-                Settings
-              </li>
-              </NavLink>
-
-
-              <li
-                onClick={
-                  () => {
-                    localStorage.clear();
-                    window.location.href = '/login';
-                  }
-                }
-              className='list-none underline text-start cursor-pointer'>
-                Logout
-              </li>
-
-            </div>
-          </div>
-
+          <NavLink to="/accountsettings" className="relative flex items-center gap-2 rounded-md bg-blue-100 px-5 py-2 hover:bg-blue-300 font-Josefin-Sans cursor-pointers mt-1 text-sm font-bold uppercase">
+            Account
+            <IoPersonOutline className="mb-1 text-lg font-semibold" />
+          </NavLink>
         </div>
 
 
