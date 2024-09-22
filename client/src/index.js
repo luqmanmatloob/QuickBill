a9c7b139d4add149667c65c2c79ce8e84f2c1b36
Luqman Matloob <93182189+luqmanmatloob@users.noreply.github.com>
2024-09-22 13:02:17 +0500

    mobile devices message

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    {/* Message for Mobile Devices */}
+    <div className="flex h-screen w-full items-center justify-center md:hidden">
+      <div className="max-w-[70vw] px-4 text-center">
+        This app is currently limited to desktop devices.
+        </div>
+    </div>
+
+    {/* App for Desktop Devices */}
+    <div className="hidden md:block">
+      <App />
+    </div>
+  </React.StrictMode>
+)
+
+reportWebVitals()
