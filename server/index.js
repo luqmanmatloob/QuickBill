ea5520aa003488eb1990b5d2648326549e7d2d02
Luqman Matloob <93182189+luqmanmatloob@users.noreply.github.com>
2025-03-05 05:50:51 +0500

    .env.example added

--- server/index.js
+++ server/index.js
@@ -32,15 +32,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
-
-
-
-// seeding admin
-createAdminUser();
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('✅ MongoDB connected');
+  
+// seeding
+  createAdminUser();
+}).catch(err => console.error('❌ MongoDB connection error:', err));
 
 
 
--- server/seeding/seedadmin.js
+++ server/seeding/seedadmin.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const User = require('../models/userModel'); // Adjust path if necessary
+const User = require('../models/userModel'); 
 
 const createAdminUser = async () => {
     try {
@@ -11,18 +11,18 @@ const createAdminUser = async () => {
 
             const adminUser = new User({
                 username: 'admin',
-                password: hashedPassword,
+                password: 123, // FIX: Use hashed password
                 secretKey: '123',
                 jwtSecretKeyAppend: 1
             });
 
             await adminUser.save();
-            console.log('Admin user created successfully.');
+            console.log('✅ Admin user created successfully.');
         } else {
-            console.log('Admin user already exists.');
+            console.log('⚠️ Admin user already exists.');
         }
     } catch (error) {
-        console.error('Error creating admin user:', error);
+        console.error('❌ Error creating admin user:', error);
     }
 };
 
